874c8e825cf2096afd621d9fe76e19c723e6ba99
karimElsaid <kelsaid233@gmail.com>
2022-12-20 00:12:29 +0200

    karim

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 
-// function y(){
-//  <!-- <i onclick=" document.getElementById('a').style.display='none' " ; -->
-// }
+let on_type1 = document.getElementById("type1-article1");
+let off1_type1 = document.getElementById("type1-article2");
+let on_type2 = document.getElementById("type2-article1");
+let off1_type2 = document.getElementById("type2-article2");
+let on_type3 = document.getElementById("type3-article1");
+let off1_type3 = document.getElementById("type3-article2");
+let on_type4 = document.getElementById("type4-article1");
+let off1_type4 = document.getElementById("type4-article2");
+function type1() {
+  on_type1.style.display = "none";
+  off1_type1.style.display = "grid";
+}
+function type2() {
+  on_type2.style.display = "none";
+  off1_type2.style.display = "grid";
+}
+function type3() {
+  on_type3.style.display = "none";
+  off1_type3.style.display = "grid";
+}
+function type4() {
+  on_type4.style.display = "none";
+  off1_type4.style.display = "grid";
+}
